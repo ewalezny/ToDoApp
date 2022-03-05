28ebf703a60f30dda021e9146cc039ad68114cd2
ewalezny <ewa.lezny@gmail.com>
2022-03-05 15:45:48 +0100

    New Task, Task, Operations, Operation js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getTasks } from "./API/tasks";
+import NewTask from "./components/NewTask";
+import Task from "./components/Task";
 
-function App() {
-  return (
-    <>
+const App = () => {
+    const [tasks, setTasks] = useState([]);
 
-    </>
-  );
+    useEffect(() => {
+        getTasks(setTasks)
+    }, [])
+
+    const handleAddNewTask = task => {
+        setTasks(prevState => {
+            return [
+                task,
+                ...prevState
+            ]
+        })
+    }
+
+    const handleRemoveTask = id => {
+        setTasks(prevState => {
+            prevState.filter(task => task.id !== id)
+        })
+    }
+
+    return (
+        <>
+            <NewTask onAddNewTask={handleAddNewTask}/>
+            {tasks.map(task => <Task key={task.id} {...task} onRemoveTask={handleRemoveTask} />)}
+        </>
+    );
 }
 
 export default App;
